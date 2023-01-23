8e4270565641b326a88c197dfe603582ec20e799
kareem <kareemalmasri9@gmail.com>
2023-01-22 19:39:21 -0500

    init push

---
+++ app.js
@@ -0,0 +1,33 @@
+const { TwitterApi } = require('twitter-api-v2');
+const dotenv = require('dotenv')
+const { Webhook } = require('discord-webhook-node');
+dotenv.config()
+
+
+const { createClient } = require('redis');
+
+const client = createClient();
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+client.connect();
+
+const hook = new Webhook(process.env.WEBHOOK_URL)
+const appOnlyClient = new TwitterApi(process.env.BEARER_TOKEN_TWITTER)
+
+async function checkTweets() {
+    const user = await appOnlyClient.v2.userByUsername("NAFGCTO")
+    const timeline = await appOnlyClient.v2.userTimeline(user.data.id, { max_results: 5 })
+    const fullTweet = await appOnlyClient.v2.singleTweet("1613916725363486721", { "tweet.fields": ["author_id", "referenced_tweets", "created_at"] })
+    if(fullTweet.data.referenced_tweets) return;
+    const mostRecent = await client.get("MOST_RECENT_TWEET")
+    const createdAt = new Date(fullTweet.data.created_at);
+    if(mostRecent && createdAt.getTime() <= mostRecent) return;
+    await client.set("MOST_RECENT_TWEET",createdAt.getTime())
+
+    await hook.send(`https://twitter.com/NAFGCTO/status/${fullTweet.data.id}`)
+
+}
+
+
+
+setInterval(checkTweets, 2000)
