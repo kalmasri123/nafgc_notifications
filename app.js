20be4bb7b39f817ac7a8411da5a4c81fa3ef6c2f
kareem <kareemalmasri9@gmail.com>
2023-01-22 20:11:50 -0500

    test

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ const appOnlyClient = new TwitterApi(process.env.BEARER_TOKEN_TWITTER)
 async function checkTweets() {
     const user = await appOnlyClient.v2.userByUsername("NAFGCTO")
     const timeline = await appOnlyClient.v2.userTimeline(user.data.id, { max_results: 5 })
+    const mostRecentTweet = await (await timeline.fetchNext()).data.data[0]
     const fullTweet = await appOnlyClient.v2.singleTweet("1613916725363486721", { "tweet.fields": ["author_id", "referenced_tweets", "created_at"] })
+    console.log(fullTweet)
     if(fullTweet.data.referenced_tweets) return;
     const mostRecent = await client.get("MOST_RECENT_TWEET")
     const createdAt = new Date(fullTweet.data.created_at);
