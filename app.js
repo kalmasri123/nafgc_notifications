23a5c17bc5b3cdec79ea13a1353806578d7ed406
kareem <kalmasri@me.com>
2023-03-25 01:59:21 -0400

    filtered EMEA

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ const appOnlyClient = new TwitterApi(process.env.BEARER_TOKEN_TWITTER)
 
 async function checkTweets() {
     const user = await appOnlyClient.v2.userByUsername("NAFGCTO")
-    const timeline = await appOnlyClient.v2.userTimeline(user.data.id, { max_results: 5,"tweet.fields":["referenced_tweets","author_id","created_at"] })
-    const mostRecentTweet = await (await timeline.fetchNext()).data.data.filter(el=>!el.referenced_tweets)[0]
-    if(mostRecentTweet.referenced_tweets) return;
+    const timeline = await appOnlyClient.v2.userTimeline(user.data.id, { max_results: 5, "tweet.fields": ["referenced_tweets", "author_id", "created_at", "text"] })
+    const mostRecentTweet = await (await timeline.fetchNext()).data.data.filter(el => !el.referenced_tweets && !el.text.includes("EMEA:"))[0]
+    if (mostRecentTweet.referenced_tweets) return;
 
     const mostRecent = await client.get("MOST_RECENT_TWEET")
     const createdAt = new Date(mostRecentTweet.created_at);
-    if(mostRecent && createdAt.getTime() <= mostRecent) return;
-    await client.set("MOST_RECENT_TWEET",createdAt.getTime())
+    if (mostRecent && createdAt.getTime() <= mostRecent) return;
+    await client.set("MOST_RECENT_TWEET", createdAt.getTime())
     await hook.send(`https://twitter.com/NAFGCTO/status/${mostRecentTweet.id}`)
 
 }
